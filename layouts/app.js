0229b8d6e1d1517f057e8d1e0b3920f8734bae70
KeenBiker <62759739+KeenBiker@users.noreply.github.com>
2020-04-19 18:42:55 +0100

    Added app.css & app.js

---
+++ layouts/app.js
@@ -0,0 +1,18 @@
+angular.module('MyApp', ['ngMaterial'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.imagePath = 'img/washedout.png';
+})
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+});
+
+
+/**
+Copyright 2018 Google LLC. All Rights Reserved. 
+Use of this source code is governed by an MIT-style license that can be found
+in the LICENSE file at http://material.angularjs.org/HEAD/license.
+**/
